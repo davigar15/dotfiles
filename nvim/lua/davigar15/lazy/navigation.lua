835762cbb44974e1213252da5249b3bd32265a1a
David García <davidgarcia@Davids-MacBook-Pro-2.local>
2025-05-22 06:28:00 +0200

    Add commando and update to harpoon2

--- nvim/lua/davigar15/lazy/navigation.lua
+++ nvim/lua/davigar15/lazy/navigation.lua
@@ -45,31 +45,40 @@ return {
 	},
 	{
 		"theprimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-			local mark = require("harpoon.mark")
-			local ui = require("harpoon.ui")
+			local harpoon = require("harpoon")
 
-			local map = function(keys, func, desc)
-				vim.keymap.set("n", keys, func, { desc = "Harpoon: " .. desc })
-			end
-			map("<leader>n", mark.add_file, "Add [N]ew entry")
-			map("<leader>m", ui.toggle_quick_menu, "Show [M]enu")
+			-- REQUIRED
+			harpoon:setup({
+				settings = {
+					save_on_toggle = true,
+					sync_on_ui_close = true,
+				},
+			})
+			vim.keymap.set("n", "<leader>n", function()
+				harpoon:list():add()
+			end)
+			vim.keymap.set("n", "<leader>m", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end)
 
-			map("<leader>h", function()
-				ui.nav_file(1)
-			end, "Go to file 1")
-			map("<leader>j", function()
-				ui.nav_file(2)
-			end, "Go to file 2")
-			map("<leader>k", function()
-				ui.nav_file(3)
-			end, "Go to file 3")
-			map("<leader>l", function()
-				ui.nav_file(4)
-			end, "Go to file 4")
-			map("<leader>;", function()
-				ui.nav_file(5)
-			end, "Go to file 5")
+			vim.keymap.set("n", "<leader>h", function()
+				harpoon:list():select(1)
+			end)
+			vim.keymap.set("n", "<leader>j", function()
+				harpoon:list():select(2)
+			end)
+			vim.keymap.set("n", "<leader>k", function()
+				harpoon:list():select(3)
+			end)
+			vim.keymap.set("n", "<leader>l", function()
+				harpoon:list():select(4)
+			end)
+			vim.keymap.set("n", "<leader>;", function()
+				harpoon:list():select(5)
+			end)
 		end,
 	},
 }
