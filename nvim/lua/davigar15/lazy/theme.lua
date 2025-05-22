81a8aaa189e49c46fdd44bc43ba79d6f1f9e8219
David Garcia <davigar15@gmail.com>
2025-05-22 20:57:15 +0200

    Remove italic

--- nvim/lua/davigar15/lazy/theme.lua
+++ nvim/lua/davigar15/lazy/theme.lua
@@ -1,5 +1,13 @@
 return {
     "rose-pine/neovim",
     name = "rose-pine",
-    config = function() vim.cmd("colorscheme rose-pine-moon") end,
+    config = function()
+        require("rose-pine").setup({
+            disable_background = true,
+            styles = {
+                italic = false,
+            },
+        })
+        vim.cmd("colorscheme rose-pine-moon")
+    end,
 }
