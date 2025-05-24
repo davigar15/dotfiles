75aad57600d3febd7b7bb340ccdd530f28f285c0
David Garcia <davigar15@gmail.com>
2025-05-25 00:24:22 +0200

    Use new version of commando

--- nvim/lua/davigar15/lazy/commando.lua
+++ nvim/lua/davigar15/lazy/commando.lua
@@ -10,20 +10,37 @@ return {
         config = function()
             local commando = require("commando")
 
-            -- Show commando list
-            vim.keymap.set("n", "<leader>cm", commando.show)
-
-            -- Run commands by index
-            vim.keymap.set("n", "<leader>ch", function() commando.run(1) end)
-            vim.keymap.set("n", "<leader>cj", function() commando.run(2) end)
-            vim.keymap.set("n", "<leader>ck", function() commando.run(3) end)
-            vim.keymap.set("n", "<leader>cl", function() commando.run(4) end)
-            vim.keymap.set("n", "<leader>c;", function() commando.run(5) end)
+            -- -- Show commando list
+            vim.keymap.set("n", "<leader>cm", commando.toggle_quick_menu)
+            --
+            -- -- Run commands by index
             vim.keymap.set(
                 "n",
-                "<leader>cc",
-                function() commando.run_latest() end
+                "<leader>ch",
+                function() commando.list:select(1) end
             )
+            vim.keymap.set(
+                "n",
+                "<leader>cj",
+                function() commando.list:select(2) end
+            )
+            vim.keymap.set(
+                "n",
+                "<leader>ck",
+                function() commando.list:select(3) end
+            )
+            vim.keymap.set(
+                "n",
+                "<leader>cl",
+                function() commando.list:select(4) end
+            )
+            vim.keymap.set(
+                "n",
+                "<leader>c;",
+                function() commando.list:select(5) end
+            )
+            vim.keymap.set("n", "<leader>cc", commando.run_latest)
+            -- vim.keymap.set("v", "<leader>cc", require("commando").run_selection)
         end,
     },
 }
