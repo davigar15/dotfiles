c4ec2fed207d6aab1bf3cc0078ce4637920b50a1
David Garcia <davigar15@gmail.com>
2025-05-25 05:18:48 +0200

    Add stylua.toml

--- nvim/lua/davigar15/lazy/commando.lua
+++ nvim/lua/davigar15/lazy/commando.lua
@@ -14,31 +14,11 @@ return {
             vim.keymap.set("n", "<leader>cm", commando.toggle_quick_menu)
             --
             -- -- Run commands by index
-            vim.keymap.set(
-                "n",
-                "<leader>ch",
-                function() commando.list:select(1) end
-            )
-            vim.keymap.set(
-                "n",
-                "<leader>cj",
-                function() commando.list:select(2) end
-            )
-            vim.keymap.set(
-                "n",
-                "<leader>ck",
-                function() commando.list:select(3) end
-            )
-            vim.keymap.set(
-                "n",
-                "<leader>cl",
-                function() commando.list:select(4) end
-            )
-            vim.keymap.set(
-                "n",
-                "<leader>c;",
-                function() commando.list:select(5) end
-            )
+            vim.keymap.set("n", "<leader>ch", function() commando.list:select(1) end)
+            vim.keymap.set("n", "<leader>cj", function() commando.list:select(2) end)
+            vim.keymap.set("n", "<leader>ck", function() commando.list:select(3) end)
+            vim.keymap.set("n", "<leader>cl", function() commando.list:select(4) end)
+            vim.keymap.set("n", "<leader>c;", function() commando.list:select(5) end)
             vim.keymap.set("n", "<leader>cc", commando.run_latest)
             -- vim.keymap.set("v", "<leader>cc", require("commando").run_selection)
         end,
